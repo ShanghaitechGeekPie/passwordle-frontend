ba012ce15db22d32dedce9bd4a302ff52b1a6e66
yanglinshu <nicolasvandarkholme@outlook.com>
2022-07-16 00:10:04 +0800

    Slight change of the layout.

--- src/App.vue
+++ src/App.vue
@@ -5,52 +5,71 @@ import GuessDisplay from "@/components/GuessDisplay.vue";
 import $global from "@/composables/useGlobal";
 import InputElement from "@/components/InputElement.vue";
 
-const {status, doCreateGame, doGetGame, doMakeGuess} = usePasswordle();
+const { status, doCreateGame, doGetGame, doMakeGuess } = usePasswordle();
 doGetGame();
 </script>
 
 <template>
   <div class="w-screen h-screen flex flex-col">
-    <div class="border-b w-full flex justify-center border-gray-300 py-1 md:py-2">
+    <div
+      class="border-b w-full flex justify-center border-gray-300 py-1 md:py-2"
+    >
       <h1 class="text-3xl md:text-4xl font-bold font-mono">Passwordle</h1>
     </div>
-    <div class="flex flex-col items-center justify-center gap-[1vh] md:gap-[2vh] flex-grow text-lg md:text-xl">
+    <div
+      class="
+        flex flex-col
+        items-center
+        justify-center
+        gap-[1vh]
+        md:gap-[2vh]
+        flex-grow
+        text-lg
+        md:text-xl
+      "
+    >
       <div>
-        <GuessDisplay :guess="$global.result"/>
+        <GuessDisplay :guess="$global.result" />
       </div>
       <div class="">
-        <div v-if="!$global.id">
-          <button class="button-start button-start-hover" @click="doCreateGame()">
-            Start
-          </button>
-        </div>
-        <div class="h-10 flex-center flex-col message" v-else>
+        <div class="h-10 flex-center flex-col message" v-if="$global.id">
           <div>{{ $global.steps }}/64</div>
         </div>
       </div>
-      <div :class="['select-none flex flex-col md:flex-row items-center font-mono', {'opacity-0':!$global.id}]">
+      <div
+        class="select-none flex flex-col md:flex-row items-center font-mono"
+        v-if="$global.id"
+      >
         <div class="md:contents">
           <span>md5('</span>
           <InputElement
-              v-for="i in new Array(8).fill(0).map((_, j) => j)"
-              :index="i"
-              :key="i"
-              :filter="(str: string) => /^[a-z0-9A-Z]$/.test(str)"
-              @finish="(status.surmise = $event), doMakeGuess()"
+            v-for="i in new Array(8).fill(0).map((_, j) => j)"
+            :index="i"
+            :key="i"
+            :filter="(str: string) => /^[a-z0-9A-Z]$/.test(str)"
+            @finish="(status.surmise = $event), doMakeGuess()"
           /><span>'&nbsp;</span>
         </div>
         <div class="md:contents">
           <span>+ '{{ $global.salt }}')</span>
         </div>
       </div>
+      <div class="h-15 pt-8" v-else>
+        <button class="button-start button-start-hover" @click="doCreateGame()">
+          Start
+        </button>
+      </div>
       <div>
-        <div class="h-8 message" v-if="status.loading"></div>
+        <div class="h-8 message" v-if="status.loading">loading...</div>
         <div class="h-8 message" v-else-if="!status.error">
           {{ status.error }}
         </div>
         <div class="h-8" v-else></div>
       </div>
-      <Keyboard :class="[{'opacity-0':!$global.id}]"/>
+      <div>
+        <Keyboard v-if="$global.id" />
+        <div class="mt-64" v-else></div>
+      </div>      
     </div>
   </div>
 </template>
